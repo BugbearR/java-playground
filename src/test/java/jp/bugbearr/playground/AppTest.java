a34ef72571f995281403233aa478ea3498870843
BugbearR <bugbearr@bugbearr.jp>
2021-12-25 21:43:31 +0900

    first commit

---
+++ src/test/java/jp/bugbearr/playground/AppTest.java
@@ -0,0 +1,20 @@
+package jp.bugbearr.playground;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
